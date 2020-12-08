3d1cb406d7f8ac1ea8907eca1aacd3585ab85a69
mohammadrezarezvan75 <mohammadrezagit75@gmail.com>
2020-12-08 09:03:54 -0800

    new part java

---
+++ project Java Script/j26/MyScript/Script.js
@@ -0,0 +1,23 @@
+// var width = window.innerWidth || document.documentElement.clientWidth ||
+//     document.body.clientWidth;
+
+// var height = window.innerHeight || document.documentElement.innerHeight ||
+//     document.body.clientHeight;
+
+
+
+// document.getElementById('demo').innerHTML = " brower width and High -" + " Width :" + width + " " + " Height :" + height;
+
+
+// document.getElementById('demo').innerHTML = screen.availHeight;
+// document.getElementById('demo1').innerHTML = screen.availWidth;
+
+// document.getElementById('demo').innerHTML = window.location.href;
+
+// document.getElementById('demo').innerHTML = window.location.pathname;
+
+// document.getElementById('demo').innerHTML = window.location.protocol;
+
+function myfunction() {
+    window.location.assign("https:wwww.google.com");
+}
